a5190977bca438c0111f8173340478e0dc549d1e
christianmspence <103942566+christianmspence@users.noreply.github.com>
2022-06-11 02:23:18 -0400

    current weather displaying on page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,34 @@
-var getCityWeather = function(city){
+var weatherContainerEl = document.querySelector("#current-container");
+
+var getCityWeather = function (city) {
     var apiKey = "dcbc862bcf16c64fe76c5467e987cbd0"
     var apiURL = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=imperial&appid=${apiKey}`
 
     fetch(apiURL)
-.then((response) => response.json())
-.then((data) => console.log(data));
+        .then((response) => response.json())
+        .then((data) => displayWeather(data))
+        .catch((error) => console.log(error));
 };
+
+var displayWeather = function (weather) {
+    var temperatureEl = document.createElement("span");
+    temperatureEl.textContent = "Temperature: " + weather.main.temp + " °F";
+    temperatureEl.classList = "list-group-item";
+
+    weatherContainerEl.appendChild(temperatureEl);
+
+    var windEl = document.createElement("span");
+    windEl.textContent = "Wind: " + weather.wind.speed + " MPH";
+    windEl.classList = "list-group-item";
+
+    weatherContainerEl.appendChild(windEl);
+
+    var humidityEl = document.createElement("span");
+    humidityEl.textContent = "Humidity " + weather.main.humidity + "%";
+    humidityEl.classList = "list-group-item";
+
+    weatherContainerEl.appendChild(humidityEl);
+
+
+}
+
